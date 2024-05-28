acc5dc561b1c42dcb4c24fdb0988f513786fe8c1
Lunar <161249671+LunarrVoice@users.noreply.github.com>
2024-05-27 22:50:08 -0400

    another code update

--- main.py
+++ main.py
@@ -22,6 +22,14 @@ solution = random.choice(word_lists.SOLUTION_LIST).upper()
 
 board = []
 typed = []
+keyboard = []
+
+KEYS = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', None],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M', None, None, None]
+]
+
 
 class Square:
     def __init__(self, name, x, y, value="empty"):
@@ -45,8 +53,35 @@ class Square:
         draw_text(SCREEN, text, WHITE, self.x + 35, self.y + 35, 44)
         
      
+class Keyboard:
+    def __init__(self, name, key, x, y, key_width, key_height, value="empty"):
+        self.name = name
+        self.key = key
+        self.x = x 
+        self.y = y
+        self.key_width = key_width
+        self.key_height = key_height
+        self.value = value
+        
+    def draw_keyboard(self):
+        if self.value == "empty":
+            draw_rounded_rect(SCREEN, LIGHTGREY, (self.x, self.y, self.key_width, self.key_height), 6)
+        elif self.value == "green":
+            draw_rounded_rect(SCREEN, GREEN, (self.x, self.y, self.key_width, self.key_height), 6)
+        elif self.value == "yellow":
+            draw_rounded_rect(SCREEN, YELLOW, (self.x, self.y, self.key_width, self.key_height), 6)
+        elif self.value == "grey":
+            draw_rounded_rect(SCREEN, GREY, (self.x, self.y, self.key_width, self.key_height), 6)
+        draw_text(SCREEN, self.key, WHITE, self.x + 25, self.y + 27, 24)
+                
+    def get_key_at_pos(self, pos):
+        x, y = pos
+        if self.x <= x <= self.x + self.key_width and self.y <= y <= self.y + self.key_height:
+            return self.key
+             
+        
 def init_board(letters, guesses):
-    global board, typed
+    global board
     
     board = [[None for x in range(letters)] for x in range(guesses)]
     
@@ -55,6 +90,28 @@ def init_board(letters, guesses):
             object_name = f"square{letter + 1}{guess + 1}"
             board[guess][letter] = Square(object_name, letter + 1, guess + 1)
     
+    
+def init_keyboard():
+    global keyboard
+    
+    keyboard = [[None for x in range(10)] for x in range(3)]
+    
+    start_x = 120
+    start_y = 520 
+    key_width = 50 
+    key_height = 57
+    margin = 7
+    stagger_offset=30
+    
+    for row in range(3):
+        row_start_x = start_x + row * stagger_offset
+        for col in range(10):
+            x = row_start_x + (key_width + margin) * col
+            y = start_y + (key_height + margin) * row
+            key = KEYS[row][col]
+            object_name = f"key{col}{row}"
+            keyboard[row][col] = Keyboard(object_name, key, x, y, key_width, key_height)
+    
                
 def update_board():
     for x, row in enumerate(board):
@@ -62,6 +119,15 @@ def update_board():
             obj.draw_square()
             obj.draw_text(typed[x][y])
                
+
+def update_keyboard():
+    for x, row in enumerate(keyboard):
+        for y, obj in enumerate(row):
+            if KEYS[x][y] == None:
+                pass
+            else:
+                obj.draw_keyboard()
+
                     
 def draw_rounded_rect(surface, color, rect, radius):
     x, y, w, h = rect
@@ -118,6 +184,13 @@ def check(guess_input, guess_number):
                     if y == guess_number:
                         if z == x:
                             obj.value = "grey"
+                            letters[x] = "grey"
+
+    for x in range(5):
+        for y, row in enumerate(keyboard):
+            for z, obj in enumerate(row):
+                if guess_input[x] == KEYS[y][z]:
+                    obj.value = letters[x]
 
     if letters == ["green", "green", "green", "green", "green"]:
         return "win"
@@ -127,14 +200,15 @@ def check(guess_input, guess_number):
 
 def main():
     global board, typed
-    
+
     typed = [[None for x in range(5)] for x in range(6)]
     guess_number = 0
     init_board(5, 6)
+    init_keyboard()
     
     while True:
         SCREEN.fill(BACKGROUND)
-
+        
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
@@ -187,8 +261,27 @@ def main():
                                     break
                             if found:
                                 break
-        
+                            
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1: 
+                    pos = pygame.mouse.get_pos()
+                    for row in keyboard:
+                        for obj in row:
+                            key = obj.get_key_at_pos(pos)
+                    if key:
+                        found = False
+                        for x, row in enumerate(typed):
+                            for y, element in enumerate(row):
+                                if element is None:
+                                    if x == guess_number:
+                                        typed[x][y] = key.capitalize()
+                                    found = True
+                                    break
+                            if found:
+                                break
+    
         update_board()
+        update_keyboard()
         
         pygame.display.flip()
 
